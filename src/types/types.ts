6fc960656d7ff807762da2deb9bdc579df8ba95d
Jadna Santos <jadnajesus7@gmail.com>
2025-10-14 18:18:49 -0300

    feat: schema to plan request

---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const DietPlanRequestSchema = z.object({
+  name: z.string().min(2),
+  age: z.number().positive(),
+  height: z.number().positive(),
+  weight: z.number().positive(),
+  activityLevel: z.enum([
+    "sedentary",
+    "two times a week",
+    "three times a week",
+    "for times a week",
+    "five times a week",
+    "six time a week",
+  ]),
+  objective: z.enum([
+    "lose weight",
+    "gain muscle mass",
+    "maintain weight",
+    "hypertrophy",
+  ]),
+  intolerances: z.string().optional(),
+  restrictions: z.string().optional(),
+  gender: z.enum(["Male", "Female"]),
+});
+
+export type DietPlanRequest = z.infer<typeof DietPlanRequestSchema>;
