211c492e85a8142a40d910dc0fc235bfbf995e5e
Jadna Santos <jadnajesus7@gmail.com>
2025-10-15 20:27:33 -0300

    feat: app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PagesModule],
+  imports: [BrowserModule, PagesModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
