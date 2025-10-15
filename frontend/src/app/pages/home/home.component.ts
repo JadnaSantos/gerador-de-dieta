0b57f56eb36f966f3ad60077187089f7ed3dd514
Jadna Santos <jadnajesus7@gmail.com>
2025-10-15 13:37:20 -0300

    feat: home page component

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { FormComponent } from 'src/app/components/form/form.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
