4c2450c73967acbcb3c526985b10b4de6383717c
Jadna Santos <jadnajesus7@gmail.com>
2025-10-16 14:51:20 -0300

    chore: add lib markdow and toast

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PagesModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    PagesModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-right',
+      newestOnTop: true,
+      preventDuplicates: true,
+      closeButton: true,
+      progressBar: true,
+      progressAnimation: 'increasing',
+      timeOut: 2500,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -4,10 +4,11 @@ import { FormComponent } from './form/form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { GeneratorComponent } from './generator/generator.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [ButtonComponent, FormComponent, GeneratorComponent],
   exports: [ButtonComponent, FormComponent, GeneratorComponent],
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, MarkdownModule.forRoot()],
 })
 export class ComponentsModule {}
